c00df92ee7fcb338924bb93c943806303b9632e3
UsmaraSana <l215779@lhr.nu.edu.pk>
2023-11-02 22:14:24 +0500

    1st commit

---
+++ src/main/java/com/mycompany/newproject/NewProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.newproject;
+
+/**
+ *
+ * @author hp
+ */
+public class NewProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
